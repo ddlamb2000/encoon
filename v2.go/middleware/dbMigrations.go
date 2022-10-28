253c6c48b2f2a3295798afaa2cb42a485e09046c
David Lambert <david.lambert@workday.com>
2022-10-28 21:14:52 +0200

    Enhance migrations.

--- v2.go/middleware/dbMigrations.go
+++ v2.go/middleware/dbMigrations.go
@@ -11,6 +11,12 @@ import (
 )
 
 func migrateDb(ctx context.Context, db *sql.DB, dbName string) {
+	if latestMigration, error := migrateInitializationDb(ctx, db, dbName); !error && latestMigration > 0 {
+		migrateDataModelDb(ctx, db, dbName, latestMigration)
+	}
+}
+
+func migrateInitializationDb(ctx context.Context, db *sql.DB, dbName string) (int, bool) {
 	var latestMigration int
 	if err := db.QueryRow("SELECT 1 FROM migrations").Scan(&latestMigration); err != nil {
 		if err == sql.ErrNoRows {
@@ -21,28 +27,69 @@ func migrateDb(ctx context.Context, db *sql.DB, dbName string) {
 			_, err := db.Exec(command)
 			if err != nil {
 				utils.LogError("[%q] Create table migrations: %v", dbName, err)
-				return
+				return latestMigration, true
 			}
 			utils.Log("[%q] Migration table created.", dbName)
 			_, err = db.Exec("INSERT INTO migrations (migration, command) VALUES ($1, $2)", 1, command)
 			if err != nil {
 				utils.LogError("[%q] Insert into migrations: %v", dbName, err)
+				return latestMigration, true
 			}
 		}
 	}
 
 	if err := db.QueryRow("SELECT MAX(migration) FROM migrations").Scan(&latestMigration); err != nil {
 		utils.LogError("[%q] Can't access migrations table %v.", dbName, err)
-		return
-	} else {
-		utils.Log("[%q] Latest migration: %v.", dbName, latestMigration)
-		migrageDbCommand(ctx, db, latestMigration, 2, "CREATE TABLE users (uuid text, version integer, enabled boolean, id text, firstName text, lastName text, password text)", dbName)
-		root, password := utils.GetRootAndPassowrd(dbName)
-		migrageDbCommand(ctx, db, latestMigration, 3, "INSERT INTO users (uuid, version, enabled, id, password) VALUES ('0', 1, true, '"+root+"', '"+password+"')", dbName)
+		return latestMigration, true
+	}
+	return latestMigration, false
+}
+
+func migrateDataModelDb(ctx context.Context, db *sql.DB, dbName string, latestMigration int) {
+	root, password := utils.GetRootAndPassowrd(dbName)
+
+	migrateCommandsDb(ctx, db, dbName, latestMigration,
+		map[int]string{
+			2: "CREATE TABLE users (" +
+				"uuid uuid, " +
+				"version integer, " +
+				"created timestamp with time zone, " +
+				"updated timestamp with time zone, " +
+				"enabled boolean, " +
+				"id text, " +
+				"firstName text, " +
+				"lastName text, " +
+				"password text)",
+
+			3: "INSERT INTO users " +
+				"(uuid, " +
+				"version, " +
+				"created, " +
+				"updated, " +
+				"enabled, " +
+				"id, " +
+				"firstName, " +
+				"lastName, " +
+				"password) " +
+				"VALUES ('3a33485c-7683-4482-aa5d-0aa51e58d79d'," +
+				" 1, " +
+				"'January 8 04:05:06 1999 PST', " +
+				"'January 8 04:05:06 1999 PST', " +
+				"true, " +
+				"'" + root + "', " +
+				"'" + root + "', " +
+				"'" + root + "', " +
+				"'" + password + "')",
+		})
+}
+
+func migrateCommandsDb(ctx context.Context, db *sql.DB, dbName string, latestMigration int, commands map[int]string) {
+	for step, command := range commands {
+		migrateDbCommand(ctx, db, latestMigration, step, command, dbName)
 	}
 }
 
-func migrageDbCommand(ctx context.Context, db *sql.DB, latestMigration int, migration int, command string, dbName string) {
+func migrateDbCommand(ctx context.Context, db *sql.DB, latestMigration int, migration int, command string, dbName string) {
 	if migration > latestMigration {
 		_, err := db.Exec(command)
 		if err != nil {
@@ -52,7 +99,7 @@ func migrageDbCommand(ctx context.Context, db *sql.DB, latestMigration int, migr
 			if err != nil {
 				utils.LogError("[%q] Insert into migrations: %v", dbName, err)
 			} else {
-				utils.Log("[%q] Latest migration: %v.", dbName, migration)
+				utils.Log("[%q] Migration: %v.", dbName, migration)
 			}
 		}
 	}
