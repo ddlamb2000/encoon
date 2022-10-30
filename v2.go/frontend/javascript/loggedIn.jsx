c65ad35e5f643b92d2986b5d80f842d5a0143fc0
David Lambert <david.lambert@workday.com>
2022-10-30 11:41:50 +0100

    Cosmetic code changes.

--- v2.go/frontend/javascript/loggedIn.jsx
+++ v2.go/frontend/javascript/loggedIn.jsx
@@ -2,154 +2,140 @@
 // Copyright David Lambert 2022
 
 class LoggedIn extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-          token: localStorage.getItem(`access_token_${dbName}`),
-          user: "",
-          userUuid: "",
-          userFirstName: "",
-          userLastName: "",
-          error: false,
-          disconnect: false,
-          isLoaded: false,
-          message: "",
-          items: [],
-      }
-      const payload = this.parseJwt(this.state.token)
-      this.state.user = payload.user
-      this.state.userUuid = payload.userUuid
-      this.state.userFirstName = payload.userFirstName
-      this.state.userLastName = payload.userLastName
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			token: localStorage.getItem(`access_token_${dbName}`),
+			user: "",
+			userUuid: "",
+			userFirstName: "",
+			userLastName: "",
+			error: false,
+			disconnect: false,
+			isLoaded: false,
+			message: "",
+			items: [],
+		}
+		const payload = this.parseJwt(this.state.token)
+		this.state.user = payload.user
+		this.state.userUuid = payload.userUuid
+		this.state.userFirstName = payload.userFirstName
+		this.state.userLastName = payload.userLastName
+	}
   
-    parseJwt(token) {
-        const base64Url = token.split('.')[1]
-        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
-        const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
-        }).join(''))
-        const parsedJsonPayload = JSON.parse(jsonPayload)
-        return parsedJsonPayload
-    }
+	parseJwt(token) {
+		const base64Url = token.split('.')[1]
+		const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+		const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+			return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+		}).join(''))
+		const parsedJsonPayload = JSON.parse(jsonPayload)
+		return parsedJsonPayload
+	}
 
-    componentDidMount() {
-        const uri = `/${dbName}/api/v1/${gridUri !== "" ? gridUri : 'users'}${uuid !== "" ? '/' + uuid : ''}`
-        fetch(uri, {
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-              'Authorization': 'Bearer ' + this.state.token
-            }
-          })
-          .then(res => res.json())
-          .then(
-              (result) => {
-                  this.setState({
-                      isLoaded: true,
-                      items: result.items,
-                      error: result.error,
-                      message: result.message,
-                      disconnect: result.disconnect
-                  })
-              },
-              (error) => {
-                  this.setState({
-                      isLoaded: false,
-                      items: [],
-                      message: `Something happened: ${error}.`,
-                      error: true
-                  })
-              }
-          )
-    }
+	componentDidMount() {
+		const uri = `/${dbName}/api/v1/${gridUri !== "" ? gridUri : 'users'}${uuid !== "" ? '/' + uuid : ''}`
+		fetch(uri, {
+			headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+			'Authorization': 'Bearer ' + this.state.token
+			}
+		})
+		.then(res => res.json())
+		.then(
+			(result) => {
+				this.setState({
+				isLoaded: true,
+				items: result.items,
+				error: result.error,
+				message: result.message,
+				disconnect: result.disconnect
+				})
+			},
+			(error) => {
+				this.setState({
+				isLoaded: false,
+				items: [],
+				message: `Something happened: ${error}.`,
+				error: true
+				})
+			}
+		)
+	}
 
-    render() {
-      const { items, isLoaded, error, disconnect } = this.state
-  
-      if(error) {
-        alert(`${dbName}: ${this.state.message}`)
-        if(disconnect) {
-            localStorage.removeItem(`access_token_${dbName}`)
-            location.reload()
-        }
-      } else if (!isLoaded) {
-          return <div>Loading…</div>
-      } else if (items == undefined) {
-          return <div>No data</div>
-      } else {
-          if(uuid !== "") {
-            const item = items[0]
-              return (
-                  <div className="container-fluid">
-                      <Navigation user={this.state.user} userFirstName={this.state.userFirstName} userLastName={this.state.userLastName} />
-                      <h2>{gridUri}</h2>
-                      <table className="table table-hover table-sm">
-                          <thead className="table-light">
-                          </thead>
-                          <tbody>
-                              <tr><td>Uuid</td><td>{item.uuid}</td></tr>
-                              <tr><td>Uri</td><td>{item.uri}</td></tr>
-                              <tr><td>Text01</td><td>{item.text01}</td></tr>
-                              <tr><td>Text02</td><td>{item.text02}</td></tr>
-                              <tr><td>Text03</td><td>{item.text03}</td></tr>
-                              <tr><td>Text04</td><td>{item.text04}</td></tr>
-                          </tbody>
-                      </table>
-                  </div>
-              )
-          }
-          else {
-                return (
-                  <div className="container-fluid">
-                      <Navigation user={this.state.user} userFirstName={this.state.userFirstName} userLastName={this.state.userLastName} />
-                      <h2>{gridUri}</h2>
-                      <table className="table table-hover table-sm">
-                          <thead className="table-light">
-                              <tr>
-                                  <th scope="col">Uri</th>
-                                  <th scope="col">Text01</th>
-                                  <th scope="col">Text02</th>
-                                  <th scope="col">Text03</th>
-                                  <th scope="col">Text04</th>
-                                  <th scope="col">
-                                    <img src="/icons/plus-circle.svg" role="img" alt="Plus circle"></img>
-                                  </th>
-                              </tr>
-                          </thead>
-                          <tbody>
-                              {items.map(item => (
-                                  <tr key={item.uuid}>
-                                      <td>{item.uri}</td>
-                                      <td>{item.text01}</td>
-                                      <td>{item.text02}</td>
-                                      <td>{item.text03}</td>
-                                      <td>{item.text04}</td>
-                                      <td scope="row"><a href={`/${dbName}/${gridUri}/${item.uuid}`}>{item.uuid}</a></td>
-                                  </tr>
-                              ))}
-                            <tr key="new line">
-                                <td><input></input></td>
-                                <td><input></input></td>
-                                <td><input></input></td>
-                                <td><input></input></td>
-                                <td><input></input></td>
-                                <td><input></input></td>
-                            </tr>
-                            <tr key="new line 2">
-                            <td>
-                                <input></input></td>
-                                <td><input></input></td>
-                                <td><input></input></td>
-                                <td><input></input></td>
-                                <td><input></input></td>
-                                <td><input></input></td>
-                            </tr>
-                          </tbody>
-                      </table>
-                  </div>
-              )
-          }
-      }
-    }  
+	render() {
+		const { items, isLoaded, error, disconnect } = this.state
+
+		if(error) {
+			alert(`${dbName}: ${this.state.message}`)
+			if(disconnect) {
+				localStorage.removeItem(`access_token_${dbName}`)
+				location.reload()
+			}
+			return
+		}
+
+		return (
+			<div className="container-fluid">
+			<Navigation user={this.state.user} userFirstName={this.state.userFirstName} userLastName={this.state.userLastName} />
+			<h2>{gridUri}</h2>
+			{!isLoaded && 
+				<div>Loading…</div>
+			}
+			{isLoaded && items == undefined &&
+				<div>No data</div>
+			}
+			{isLoaded && items != undefined && uuid == "" &&
+				<table className="table table-hover table-sm">
+					<thead className="table-light">
+						<tr>
+							<th scope="col">Uri</th>
+							<th scope="col">Text01</th>
+							<th scope="col">Text02</th>
+							<th scope="col">Text03</th>
+							<th scope="col">Text04</th>
+							<th scope="col">
+								<img src="/icons/plus-circle.svg" role="img" alt="Plus circle"></img>
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{items.map(item => (
+							<tr key={item.uuid}>
+								<td>{item.uri}</td>
+								<td>{item.text01}</td>
+								<td>{item.text02}</td>
+								<td>{item.text03}</td>
+								<td>{item.text04}</td>
+								<td scope="row"><a href={`/${dbName}/${gridUri}/${item.uuid}`}>{item.uuid}</a></td>
+							</tr>
+						))}
+						<tr key="new line">
+							<td><input></input></td>
+							<td><input></input></td>
+							<td><input></input></td>
+							<td><input></input></td>
+							<td><input></input></td>
+							<td><input></input></td>
+						</tr>
+					</tbody>
+				</table>
+			}
+			{isLoaded && items != undefined && uuid != "" &&
+				<table className="table table-hover table-sm">
+					<thead className="table-light"></thead>
+					<tbody>
+						<tr><td>Uuid</td><td>{items[0].uuid}</td></tr>
+						<tr><td>Uri</td><td>{items[0].uri}</td></tr>
+						<tr><td>Text01</td><td>{items[0].text01}</td></tr>
+						<tr><td>Text02</td><td>{items[0].text02}</td></tr>
+						<tr><td>Text03</td><td>{items[0].text03}</td></tr>
+						<tr><td>Text04</td><td>{items[0].text04}</td></tr>
+					</tbody>
+				</table>
+			}
+			</div>
+		)
+	}
 }
--- v2.go/frontend/javascript/notLogged.jsx
+++ v2.go/frontend/javascript/notLogged.jsx
@@ -2,110 +2,107 @@
 // Copyright David Lambert 2022
 
 class NotLogged extends React.Component {
-  constructor(props) {
-    super(props)
-    this.dbNameInput = null
-    this.setDbNameRef = element => { this.dbNameInput = element }
-    this.idInput = null
-    this.setIdRef = element => { this.idInput = element }
-    this.passwordInput = null
-    this.setPasswordRef = element => { this.passwordInput = element }
+	constructor(props) {
+		super(props)
+		this.dbNameInput = null
+		this.setDbNameRef = element => { this.dbNameInput = element }
+		this.idInput = null
+		this.setIdRef = element => { this.idInput = element }
+		this.passwordInput = null
+		this.setPasswordRef = element => { this.passwordInput = element }
 
-    this.authenticate = () => {
-      const updatedDbName = dbName != '' ? dbName : this.dbNameInput.value
-      if(updatedDbName == '') {
-        alert("Database name is required.")
-        return null
-      }
-      if(this.idInput.value == '' || this.passwordInput.value == '') {
-        alert("Username and passphrase are both required.")
-        return null
-      }
-      fetch(`/${updatedDbName}/api/v1/authentication`, {
-        method: 'POST',
-        headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-        body: JSON.stringify({ id: this.idInput.value, password: btoa(this.passwordInput.value) })
-      })
-      .then( (response) => {
-        if(response.status == 400) {
-          alert("Incorrect user credentials.")
-          return null
-        }
-        if(response.status != 200) {
-          alert(`Problem ${response.status} is reported.`)
-          return null
-        }
-        return response.json() })
-      .then( (responseJson) => {
-        if (responseJson != null) {
-          localStorage.setItem(`access_token_${updatedDbName}`, responseJson.token)
-        }
-        if(dbName == '') location.href = `/${updatedDbName}/`;
-        else location.reload()
-      } )  
-    }
-  }
+		this.authenticate = () => {
+			const updatedDbName = dbName != '' ? dbName : this.dbNameInput.value
+			if(updatedDbName == '') {
+				alert("Database name is required.")
+				return null
+			}
+			if(this.idInput.value == '' || this.passwordInput.value == '') {
+				alert("Username and passphrase are both required.")
+				return null
+			}
+			fetch(`/${updatedDbName}/api/v1/authentication`, {
+				method: 'POST',
+				headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+				body: JSON.stringify({ id: this.idInput.value, password: btoa(this.passwordInput.value) })
+			})
+			.then( (response) => {
+				if(response.status == 400) {
+					alert("Incorrect user credentials.")
+					return null
+				}
+				if(response.status != 200) {
+					alert(`Problem ${response.status} is reported.`)
+					return null
+				}
+				return response.json() })
+				.then( (responseJson) => {
+					if (responseJson != null) {
+						localStorage.setItem(`access_token_${updatedDbName}`, responseJson.token)
+					}
+					if(dbName == '') location.href = `/${updatedDbName}/`
+					else location.reload()
+				} 
+			)  
+		}
+	}
 
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col"></div>
-          <div className="col">
-            <br/><br/><br/><br/><br/><br/><br/>
-            <center>
-              <h1>{appName}</h1>
-              <small className="text-muted">Data structuration, presentation and navigation.</small>
-              <br/>
-              <br/>
-              {dbName != "" && <h3>{dbName}</h3>}
-            </center>
-            {dbName == "" &&
-              <div className="mb-3">
-                <label htmlFor="dbName" className="form-label">Database</label>
-                <input
-                  type="text" 
-                  className="form-control" 
-                  id="dbName" 
-                  ref={this.setDbNameRef}/>
-              </div>
-            }
-            <div className="mb-3">
-              <label htmlFor="id" className="form-label">Username</label>
-              <input
-                type="text" 
-                className="form-control" 
-                id="id" 
-                ref={this.setIdRef}
-                aria-describedby="idHelp"/>
-              <div id="idHelp" className="form-text">We'll never share your data with anyone else.</div>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="password" className="form-label">Passphrase</label>
-              <input 
-                type="password" 
-                className="form-control" 
-                id="password"
-                ref={this.setPasswordRef}/>
-              <div id="passwordHelp" className="form-text">
-                A passphrase is a sequence of words or other text used to control access 
-                to a computer system, program or data. 
-                It is similar to a password in usage, but a passphrase is generally longer 
-                for added security.
-              </div>
-            </div>
-            <div class="d-grid gap-2">
-              <button
-                type="button" 
-                className="btn btn-outline-primary"
-                onClick={this.authenticate}>
-                  Log in {dbName} <img src="/icons/box-arrow-in-right.svg" role="img" alt="Log in" />
-              </button>
-            </div>
-          </div>
-          <div className="col"></div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col"></div>
+					<div className="col">
+						<br/><br/><br/><br/><br/><br/><br/>
+						<center>
+							<h1>{appName}</h1>
+							<small className="text-muted">Data structuration, presentation and navigation.</small>
+							<br/>
+							<br/>
+							{dbName != "" && <h3>{dbName}</h3>}
+						</center>
+						{dbName == "" &&
+							<div className="mb-3">
+								<label htmlFor="dbName" className="form-label">Database</label>
+								<input type="text" 
+									className="form-control" 
+									id="dbName" 
+									ref={this.setDbNameRef}/>
+							</div>
+						}
+						<div className="mb-3">
+							<label htmlFor="id" className="form-label">Username</label>
+							<input type="text" 
+								className="form-control" 
+								id="id" 
+								ref={this.setIdRef}
+								aria-describedby="idHelp"/>
+							<div id="idHelp" className="form-text">We'll never share your data with anyone else.</div>
+						</div>
+						<div className="mb-3">
+							<label htmlFor="password" className="form-label">Passphrase</label>
+							<input type="password" 
+								className="form-control" 
+								id="password"
+								ref={this.setPasswordRef}/>
+							<div id="passwordHelp" className="form-text">
+								A passphrase is a sequence of words or other text used to control access 
+								to a computer system, program or data. 
+								It is similar to a password in usage, but a passphrase is generally longer 
+								for added security.
+							</div>
+						</div>
+						<div className="d-grid gap-2">
+							<button type="button" 
+								className="btn btn-outline-primary"
+								onClick={this.authenticate}>
+								Log in {dbName} <img src="/icons/box-arrow-in-right.svg" role="img" alt="Log in" />
+							</button>
+						</div>
+					</div>
+					<div className="col"></div>
+				</div>
+			</div>
+		)
+	}
 }
