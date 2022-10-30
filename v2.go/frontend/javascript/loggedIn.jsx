c961d6d4d7db65be49c086d6af6c8519e592710f
David Lambert <david.lambert@workday.com>
2022-10-30 13:24:21 +0100

    Code simplification.

--- v2.go/frontend/javascript/loggedIn.jsx
+++ v2.go/frontend/javascript/loggedIn.jsx
@@ -34,7 +34,7 @@ class LoggedIn extends React.Component {
 		return parsedJsonPayload
 	}
 
-	componentWillMount() {
+	componentDidMount() {
 		this.setState({isLoading: true})
 
 		// temporisation - to be removed
@@ -78,7 +78,6 @@ class LoggedIn extends React.Component {
 
 	render() {
 		const { items, isLoading, isLoaded, error, disconnect } = this.state
-
 		if(error) {
 			alert(`${dbName}: ${this.state.message}`)
 			if(disconnect) {
@@ -87,69 +86,60 @@ class LoggedIn extends React.Component {
 			}
 			return
 		}
-
 		return (
 			<div className="container-fluid">
 				<Navigation user={this.state.user} userFirstName={this.state.userFirstName} userLastName={this.state.userLastName} />
 				<h3>{gridUri}</h3>
-				{isLoading &&
-					<div>Loading…</div>
-				}
-				{isLoaded && items == undefined &&
-					<div>No data</div>
-				}
-				{isLoaded && items != undefined && uuid != "" &&
-					<span className="text-muted">{items[0].uuid}</span>
-				}
-				{isLoaded && items != undefined && uuid == "" &&
-					<table className="table table-hover table-sm">
-						<thead className="table-light">
-							<tr>
-								<th scope="col">Uri</th>
-								<th scope="col">Text01</th>
-								<th scope="col">Text02</th>
-								<th scope="col">Text03</th>
-								<th scope="col">Text04</th>
-								<th scope="col">
-									<img src="/icons/plus-circle.svg" role="img" alt="Plus circle"></img>
-								</th>
-							</tr>
-						</thead>
-						<tbody>
-							{items.map(item => (
-								<tr key={item.uuid}>
-									<td>{item.uri}</td>
-									<td>{item.text01}</td>
-									<td>{item.text02}</td>
-									<td>{item.text03}</td>
-									<td>{item.text04}</td>
-									<td scope="row"><a href={`/${dbName}/${gridUri}/${item.uuid}`}>{item.uuid}</a></td>
-								</tr>
-							))}
-							<tr key="new line">
-								<td><input></input></td>
-								<td><input></input></td>
-								<td><input></input></td>
-								<td><input></input></td>
-								<td><input></input></td>
-								<td><input></input></td>
-							</tr>
-						</tbody>
-					</table>
-				}
-				{isLoaded && items != undefined && uuid != "" &&
-					<table className="table table-hover table-sm">
-						<thead className="table-light"></thead>
-						<tbody>
-							<tr><td>Uri</td><td>{items[0].uri}</td></tr>
-							<tr><td>Text01</td><td>{items[0].text01}</td></tr>
-							<tr><td>Text02</td><td>{items[0].text02}</td></tr>
-							<tr><td>Text03</td><td>{items[0].text03}</td></tr>
-							<tr><td>Text04</td><td>{items[0].text04}</td></tr>
-						</tbody>
-					</table>
-				}
+				{isLoading && <div>Loading…</div>}
+				{isLoaded && items == undefined && <div>No data</div>}
+				{isLoaded && items != undefined && uuid != "" && <span className="text-muted">{items[0].uuid}</span>}
+				{isLoaded && items != undefined && uuid == "" && <TableRows items={items} />}
+				{isLoaded && items != undefined && uuid != "" && <TableSingleRow item={items[0]} />}
 			</div>
 		)
 	}
 }
+
+const TableRows = ({ items }) =>
+	<table className="table table-hover table-sm">
+	<thead className="table-light">
+		<tr>
+			<th scope="col">Uri</th>
+			<th scope="col">Text01</th>
+			<th scope="col">Text02</th>
+			<th scope="col">Text03</th>
+			<th scope="col">Text04</th>
+			<th scope="col">
+				<img src="/icons/plus-circle.svg" role="img" alt="Plus circle"></img>
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		{items.map(item => <TableRowItemSingleLine key={item.uuid} item={item} />)}
+	</tbody>
+	</table>
+
+
+const TableRowItemSingleLine = ({ item }) =>
+	<tr>
+		<td>{item.uri}</td>
+		<td>{item.text01}</td>
+		<td>{item.text02}</td>
+		<td>{item.text03}</td>
+		<td>{item.text04}</td>
+		<td scope="row">
+			<a href={`/${dbName}/${gridUri}/${item.uuid}`}>{item.uuid}</a>
+		</td>
+	</tr>
+
+const TableSingleRow = ({ item }) =>
+	<table className="table table-hover table-sm">
+		<thead className="table-light"></thead>
+		<tbody>
+			<tr><td>Uri</td><td>{item.uri}</td></tr>
+			<tr><td>Text01</td><td>{item.text01}</td></tr>
+			<tr><td>Text02</td><td>{item.text02}</td></tr>
+			<tr><td>Text03</td><td>{item.text03}</td></tr>
+			<tr><td>Text04</td><td>{item.text04}</td></tr>
+		</tbody>
+	</table>
