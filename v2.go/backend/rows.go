4e96c11a8ceb898227e24a1d4811ded7614a005e
David Lambert <david.lambert@workday.com>
2022-11-10 00:24:36 +0100

    Introduced omitempty for Json struct.

--- v2.go/backend/rows.go
+++ v2.go/backend/rows.go
@@ -15,26 +15,26 @@ type Row struct {
 	GridUuid  *string `json:"gridUuid"`
 	CreateBy  *string `json:"createdBy"`
 	UpdatedBy *string `json:"updatedBy"`
-	Text01    *string `json:"text01"`
-	Text02    *string `json:"text02"`
-	Text03    *string `json:"text03"`
-	Text04    *string `json:"text04"`
-	Text05    *string `json:"text05"`
-	Text06    *string `json:"text06"`
-	Text07    *string `json:"text07"`
-	Text08    *string `json:"text08"`
-	Text09    *string `json:"text09"`
-	Text10    *string `json:"text10"`
-	Int01     *int64  `json:"int01"`
-	Int02     *int64  `json:"int02"`
-	Int03     *int64  `json:"int03"`
-	Int04     *int64  `json:"int04"`
-	Int05     *int64  `json:"int05"`
-	Int06     *int64  `json:"int06"`
-	Int07     *int64  `json:"int07"`
-	Int08     *int64  `json:"int08"`
-	Int09     *int64  `json:"int09"`
-	Int10     *int64  `json:"int10"`
+	Text01    *string `json:"text01,omitempty"`
+	Text02    *string `json:"text02,omitempty"`
+	Text03    *string `json:"text03,omitempty"`
+	Text04    *string `json:"text04,omitempty"`
+	Text05    *string `json:"text05,omitempty"`
+	Text06    *string `json:"text06,omitempty"`
+	Text07    *string `json:"text07,omitempty"`
+	Text08    *string `json:"text08,omitempty"`
+	Text09    *string `json:"text09,omitempty"`
+	Text10    *string `json:"text10,omitempty"`
+	Int01     *int64  `json:"int01,omitempty"`
+	Int02     *int64  `json:"int02,omitempty"`
+	Int03     *int64  `json:"int03,omitempty"`
+	Int04     *int64  `json:"int04,omitempty"`
+	Int05     *int64  `json:"int05,omitempty"`
+	Int06     *int64  `json:"int06,omitempty"`
+	Int07     *int64  `json:"int07,omitempty"`
+	Int08     *int64  `json:"int08,omitempty"`
+	Int09     *int64  `json:"int09,omitempty"`
+	Int10     *int64  `json:"int10,omitempty"`
 
 	Path string `json:"path"`
 }
