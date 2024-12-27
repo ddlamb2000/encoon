9ea3dfb9a121fffa780bed55fedb17c7ae42c18c
David Lambert <david.lambert@workday.com>
2024-12-27 13:10:40 +0100

    Refactor.

--- backend/kafka/reader.go
+++ backend/kafka/reader.go
@@ -55,73 +55,76 @@ func SetAndStartKafkaReaderForDatabase(dbName string) {
 
 	configuration.Log("", "", "Read messages on topic %s through brokers %s with consumer group %s.", topic, kafkaBrokers, groupID)
 	for {
-		m, err := consumer.FetchMessage(context.Background())
+		message, err := consumer.FetchMessage(context.Background())
 		if err != nil {
 			configuration.LogError("", "", "could not read message: %v", err)
 			continue
 		}
-
-		err = consumer.CommitMessages(context.Background(), m)
+		err = consumer.CommitMessages(context.Background(), message)
 		if err != nil {
-			configuration.LogError("", "", "failed to commit message from topic %s, partition %d and offset %d", m.Topic, m.Partition, m.Offset)
+			configuration.LogError("", "", "failed to commit message from topic %s, partition %d and offset %d", message.Topic, message.Partition, message.Offset)
 		} else {
-			configuration.Log("", "", "Got: topic: %s, key: %s, value: %s, headers: %s", m.Topic, m.Key, m.Value, m.Headers)
-
-			initiatedOn := []byte("")
-			for _, header := range m.Headers {
-				if header.Key == "initiatedOn" {
-					initiatedOn = header.Value
-				}
-			}
-
-			var content requestContent
-			if err = json.Unmarshal(m.Value, &content); err != nil {
-				configuration.LogError("", "", "Error unmarshal message value", err)
-				continue
-			}
-
-			var response responseContent
-			if content.Action == "login" {
-				configuration.Log("", "", "try to login %s, %s, %s", content.Dbname, content.Userid, content.Password)
-				userUuid, firstName, lastName, timeOut, err := database.IsDbAuthorized(context.Background(), content.Dbname, content.Userid, content.Password)
-				configuration.Log("", "", " %s, %s, %s", userUuid, firstName, lastName)
-				if err != nil || userUuid == "" {
-					if timeOut {
-						response = responseContent{
-							Status:       "KO",
-							ErrorMessage: "Timeout",
-						}
-
-					} else {
-						response = responseContent{
-							Status:       "KO",
-							ErrorMessage: "Invalid username or passphrase",
-						}
-					}
-				} else {
-					response = responseContent{
-						Status: "OK",
-					}
-				}
-			} else {
-				response = responseContent{
-					Status:   "OK",
-					Action:   content.Action,
-					GridUuid: content.GridUuid,
-					RowUuid:  content.RowUuid,
-				}
-			}
-
-			responseEncoded, err := json.Marshal(response)
-			if err != nil {
-				configuration.LogError("", "", "error marshal response:", err)
-				continue
-			}
-			WriteMessage(dbName, m.Key, initiatedOn, responseEncoded)
+			handleMessage(dbName, message)
 		}
 	}
 
 	if err := consumer.Close(); err != nil {
 		configuration.LogError("", "", "Failed to close reader:", err)
 	}
+}
+
+func handleMessage(dbName string, message kafka.Message) {
+	configuration.Log("", "", "Got: topic: %s, key: %s, value: %s, headers: %s", message.Topic, message.Key, message.Value, message.Headers)
+
+	initiatedOn := []byte("")
+	for _, header := range message.Headers {
+		if header.Key == "initiatedOn" {
+			initiatedOn = header.Value
+		}
+	}
+
+	var content requestContent
+	if err := json.Unmarshal(message.Value, &content); err != nil {
+		configuration.LogError("", "", "Error unmarshal message value", err)
+		return
+	}
+
+	var response responseContent
+	if content.Action == "login" {
+		configuration.Log("", "", "try to login %s, %s, %s", content.Dbname, content.Userid, content.Password)
+		userUuid, firstName, lastName, timeOut, err := database.IsDbAuthorized(context.Background(), content.Dbname, content.Userid, content.Password)
+		configuration.Log("", "", " %s, %s, %s", userUuid, firstName, lastName)
+		if err != nil || userUuid == "" {
+			if timeOut {
+				response = responseContent{
+					Status:       "KO",
+					ErrorMessage: "Timeout",
+				}
+
+			} else {
+				response = responseContent{
+					Status:       "KO",
+					ErrorMessage: "Invalid username or passphrase",
+				}
+			}
+		} else {
+			response = responseContent{
+				Status: "OK",
+			}
+		}
+	} else {
+		response = responseContent{
+			Status:   "OK",
+			Action:   content.Action,
+			GridUuid: content.GridUuid,
+			RowUuid:  content.RowUuid,
+		}
+	}
+
+	responseEncoded, err := json.Marshal(response)
+	if err != nil {
+		configuration.LogError("", "", "error marshal response:", err)
+		return
+	}
+	WriteMessage(dbName, message.Key, initiatedOn, responseEncoded)
 }
