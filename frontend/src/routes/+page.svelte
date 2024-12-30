accf43386587a96bb93abf3a84edacbbd4b9b6c2
David Lambert <david.lambert@workday.com>
2024-12-30 17:39:27 +0100

    Frontend cleanup.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -9,6 +9,28 @@
 
 <section>
 
+	<div class="text-column">
+		<h1>εncooη</h1>
+		<p>For the time being, εncooη is an experimental web-based application 
+		oriented toward data management.</p>
+		<p>The object of the reasearches εncooη is based on is to propose 
+		a true engine that allows data structuration, data relationship 
+		management, data presentation, and, mainly, to make obvious data navigation.</p>	
+		<p>This brand new application doesn't require database system 
+		learning or skills, because everything in the application 
+		is managed through a very simple web-based user interface, 
+		in real time. </p>
+		<p>Primary dedicated to small business structures, 
+		the application lets manage a large amount of multi-purposes information 
+		within the organization, and share this information across business stakeholders 
+		in an easy and natural way, using one Internet browser only.</p>
+		<p>εncooη is also the foundation for the development of a true business-oriented 
+		application development toolkit.</p>
+		<p>Copyright David Lambert 2024</p>
+	</div>
+	
+
+
 	<Counter />
 </section>
 
