1a4d266913b70a2b0441f839a4b5e04fdcbf0f7e
David Lambert <david.lambert@workday.com>
2025-01-04 15:22:15 +0100

    Changed overall layout.

--- frontend/.svelte-kit/generated/client/nodes/4.js
+++ frontend/.svelte-kit/generated/client/nodes/4.js
@@ -1,3 +1,3 @@
-import * as universal from "../../../../src/routes/[dbName]/[[gridUuid]]/+page.ts";
+import * as universal from "../../../../src/routes/mockup/+page.ts";
 export { universal };
-export { default as component } from "../../../../src/routes/[dbName]/[[gridUuid]]/+page.svelte";
+export { default as component } from "../../../../src/routes/mockup/+page.svelte";
--- frontend/src/routes/[dbName]/[[gridUuid]]/+page.svelte
+++ frontend/src/routes/[dbName]/[[gridUuid]]/+page.svelte
@@ -3,7 +3,6 @@
   import { newUuid } from "$lib/utils.svelte"
   import * as metadata from "$lib/metadata.svelte"
   import { Indicator, Button, A } from 'flowbite-svelte'
-  import { Drawer, Sidebar, SidebarWrapper, SidebarGroup, Footer } from 'flowbite-svelte';
   import { fade } from 'svelte/transition'
   import { onMount, onDestroy } from 'svelte'
   import { Context } from './context.svelte.ts'
@@ -15,8 +14,6 @@
   
   let { data }: { data: PageData } = $props()
   let context = $state(new Context(data.dbName, data.url, data.gridUuid))
-  let drawerHidden: boolean = false
-  let backdrop: boolean = false;
 
   onMount(() => {
     context.getStream()
@@ -38,122 +35,99 @@
 
 <svelte:head><title>εncooη - {context.dbName}</title></svelte:head>
 
-<header>
-  εncooη
-  {#if context.isStreaming}
-    {#if context && context.user && context.user.getIsLoggedIn()}
-      {context.user.getFirstName()} {context.user.getLastName()}
-      <Button size="xs" color="dark" onclick={() => context.logout()}>Log out</Button>
-    {/if}
-    <span class="inline-flex items-center"><Indicator size="sm" color="teal" class="me-1" />Connected</span>
-    {#if context.isSending}
-      <span class="inline-flex items-center"><Indicator size="sm" color="orange" class="me-1" />Sending</span>
-    {:else}
-      {#if context.messageStatus}
-        <span class="inline-flex items-center"><Indicator size="sm" color="red" class="me-1" />{context.messageStatus}</span>
-      {:else}
-        <span class="inline-flex items-center"><Indicator size="sm" color="teal" class="me-1" />OK</span>
-      {/if}
-    {/if}
-  {:else}
-    <span class="inline-flex items-center"><Indicator size="sm" color="orange" class="me-1" /></span>
-  {/if}
-</header>
 
-<section>
-  <nav>
-    <ul transition:fade>
-      {#if context.focus.grid}
-        <li>
-          <ul>
-            <li>Grid: {@html context.focus.grid.text1} ({@html context.focus.grid.text2})</li>
-            <li>Column: {context.focus.column.label} ({context.focus.column.type})</li>
-            <li>Row: {context.focus.row.displayString} ({context.focus.row.uuid})</li>
-            <li>Value: {@html context.focus.row[context.focus.column.name]}</li>
-            <li>Created on <DateTime dateTime={context.focus.row.created} /></li>
-            <li>Updated on <DateTime dateTime={context.focus.row.updated} /></li>
-          </ul>
-        </li>
-      {/if}  
-      <li><A color="text-blue-700 dark:text-blue-500" onclick={() => newGrid()}><span class="flex items-center"><Icon.CirclePlusOutline />New Grid</span></A></li>
-      <li><A color="text-blue-700 dark:text-blue-500" onclick={() => context.navigateToGrid(metadata.UuidGrids)}><span class="flex items-center"><Icon.ListOutline />Grids</span></A></li>
-      {#each context.dataSet as set}
-        {#if set.grid && set.grid.uuid && set.grid.uuid !== metadata.UuidGrids}
-          <li>
-            <a color="text-blue-700 dark:text-blue-500" href="#" onclick={() => context.navigateToGrid(set.grid.uuid)}>
-              {@html set.grid.text1}
-            </a>
-          </li>
-        {/if}
-      {/each}
-    </ul>
-  </nav>
+<main class="global-container grid h-full [grid-template-rows:auto_1fr]">
+  <nav class="global header bg-violet-200">
 
-  <article>
+    εncooη
     {#if context.isStreaming}
       {#if context && context.user && context.user.getIsLoggedIn()}
-        <div transition:fade>
-          <Grid bind:context={context} gridUuid={context.gridUuid} />
-        </div>
-      {:else}
-        <form transition:fade>
-          <label>Username<input bind:value={loginId} /></label>
-          <label>Passphrase<input bind:value={loginPassword} type="password" /></label>
-          <Button size="xs" type="submit" onclick={() => context.authentication(loginId, loginPassword)}>Log in</Button>
-        </form>
+        {context.user.getFirstName()} {context.user.getLastName()}
+        <Button size="xs" color="dark" onclick={() => context.logout()}>Log out</Button>
       {/if}
+      <span class="inline-flex items-center"><Indicator size="sm" color="teal" class="me-1" />Connected</span>
+      {#if context.isSending}
+        <span class="inline-flex items-center"><Indicator size="sm" color="orange" class="me-1" />Sending</span>
+      {:else}
+        {#if context.messageStatus}
+          <span class="inline-flex items-center"><Indicator size="sm" color="red" class="me-1" />{context.messageStatus}</span>
+        {:else}
+          <span class="inline-flex items-center"><Indicator size="sm" color="teal" class="me-1" />OK</span>
+        {/if}
+      {/if}
+    {:else}
+      <span class="inline-flex items-center"><Indicator size="sm" color="orange" class="me-1" /></span>
     {/if}
-  </article>
-</section>
+      
 
-<footer>
-  <Info {context} />
-</footer>
+
+  </nav>
+  <section class="main-container bg-sky-200 grid [grid-template-columns:1fr_4fr] overflow-auto">
+    <aside class="side-bar bg-teal-200 grid overflow-auto">
+      <div class="overflow-auto h-[500px]">
+
+        <ul transition:fade>
+          {#if context.focus.grid}
+            <li>
+              <ul>
+                <li>Grid: {@html context.focus.grid.text1} ({@html context.focus.grid.text2})</li>
+                <li>Column: {context.focus.column.label} ({context.focus.column.type})</li>
+                <li>Row: {context.focus.row.displayString} ({context.focus.row.uuid})</li>
+                <li>Value: {@html context.focus.row[context.focus.column.name]}</li>
+                <li>Created on <DateTime dateTime={context.focus.row.created} /></li>
+                <li>Updated on <DateTime dateTime={context.focus.row.updated} /></li>
+              </ul>
+            </li>
+          {/if}  
+          <li><A color="text-blue-700 dark:text-blue-500" onclick={() => newGrid()}><span class="flex items-center"><Icon.CirclePlusOutline />New Grid</span></A></li>
+          <li><A color="text-blue-700 dark:text-blue-500" onclick={() => context.navigateToGrid(metadata.UuidGrids)}><span class="flex items-center"><Icon.ListOutline />Grids</span></A></li>
+          {#each context.dataSet as set}
+            {#if set.grid && set.grid.uuid && set.grid.uuid !== metadata.UuidGrids}
+              <li>
+                <a color="text-blue-700 dark:text-blue-500" href="#" onclick={() => context.navigateToGrid(set.grid.uuid)}>
+                  {@html set.grid.text1}
+                </a>
+              </li>
+            {/if}
+          {/each}
+        </ul>        
+
+      </div>
+    </aside>
+    <section class="content bg-lime-200 grid [grid-template-rows:auto_auto_1fr_auto] overflow-auto">
+      <div class="bg-yellow-200">
+        tabs
+      </div>
+      <aside class="bg-orange-200">
+        toolbar
+      </aside>
+      <div class="bg-red-200 grid overflow-auto">
+        <article class="h-[500px]">
+
+          {#if context.isStreaming}
+            {#if context && context.user && context.user.getIsLoggedIn()}
+              <div transition:fade>
+                <Grid bind:context={context} gridUuid={context.gridUuid} />
+              </div>
+            {:else}
+              <form transition:fade>
+                <label>Username<input bind:value={loginId} /></label>
+                <label>Passphrase<input bind:value={loginPassword} type="password" /></label>
+                <Button size="xs" type="submit" onclick={() => context.authentication(loginId, loginPassword)}>Log in</Button>
+              </form>
+            {/if}
+          {/if}
+  
+
+        </article>
+      </div>
+      <footer class="bg-slate-200">
+        <Info {context} />
+      </footer>
+    </section>
+  </section>
+</main>
 
 <style>
-  @media (max-width: 600px) {
-    nav, article {
-      width: 100%;
-      height: auto;
-    }
-  }
-  * { box-sizing: border-box; }
-  li { list-style: none; }
-  header {
-    position: fixed !important;
-    width: 100%;
-    height: 32px;
-    background-color: #666;
-    padding: 3px;
-    color: white;
-  }
-  nav {
-    position: fixed !important;
-    margin-top: 32px;
-    width: 280px;
-    background: #ccc;
-    padding: 20px;
-  }
-  article {
-    /* margin-top: 32px; */
-    margin-left: 280px;
-    padding: 20px;
-    width: 100%;
-    background-color: #f1f1f1;
-    z-index: 1;
-    overflow: auto;
-  }
-  section::after {
-    content: "";
-    display: table;
-    clear: both;
-  }
-  footer {
-    position: fixed !important;
-    margin-top: 320px;
-    background-color: #777;
-    padding: 10px;
-    text-align: center;
-    color: white;
-  }
+
 </style>
