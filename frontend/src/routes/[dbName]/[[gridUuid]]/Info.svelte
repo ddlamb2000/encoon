ae98319dfe775953c363efb655da1bffd9d757e9
David Lambert <david.lambert@workday.com>
2025-01-04 07:26:49 +0100

    Simplify display of messages.

--- frontend/src/routes/[dbName]/[[gridUuid]]/Info.svelte
+++ frontend/src/routes/[dbName]/[[gridUuid]]/Info.svelte
@@ -1,41 +1,47 @@
 <script lang="ts">
-  import { Toast } from 'flowbite-svelte';
   import { DownloadOutline, PaperPlaneOutline } from 'flowbite-svelte-icons';
+  import { SuccessStatus } from '$lib/metadata.svelte'
   let { context } = $props()
 </script>
-{#each context.messageStack as message}
-  {#if message.request}
-    <Toast color="green">
-      <svelte:fragment slot="icon">
-        <PaperPlaneOutline class="w-4 h-4 rotate-45" />
-        <span class="sr-only">Error icon</span>
-      </svelte:fragment>
-      <div class="ps-4 text-xs font-normal">
-        <p><strong>Key: </strong>{message.request.messageKey}</p>
-        {#if message.request.action}<p><strong>Action: </strong>{message.request.action}</p>{/if}
-        {#if message.request.actionText}<p><strong>Text: </strong>{message.request.action}</p>{/if}
-        <p><strong>GridUuid: </strong>{message.request.gridUuid}</p>
-      </div>
-    </Toast>        
-  {/if}
-  {#if message.response}
-    <Toast color="blue">
-      <svelte:fragment slot="icon">
-        <DownloadOutline class="w-4 h-4" />
-        <span class="sr-only">Error icon</span>
-      </svelte:fragment>
-      <div class="ps-4 text-xs font-normal">
-        <p><strong>Key: </strong>{message.response.messageKey}</p>
-        {#if message.response.requestKey}<p><strong>Request: </strong>{message.response.requestKey}</p>{/if}
-        {#if message.response.action}<p><strong>Action: </strong>{message.response.action}</p>{/if}
-        {#if message.response.actionText}<p><strong>Text: </strong>{message.response.action}</p>{/if}
-        <p><strong>GridUuid: </strong>{message.response.gridUuid}</p>
-        <p><strong>Status: </strong>{message.response.status}</p>
-        <p><strong>Elasped: </strong>{message.response.elapsedMs} ms</p>
-      </div>
-    </Toast>        
-  {/if}
-{/each}
+
+<ul>
+  {#each context.messageStack as message}
+    {#if message.request}
+      <li>
+        <span class="flex items-center">
+          <PaperPlaneOutline color="blue" class="w-4 h-4 rotate-45" />
+          <div class="ps-4 text-xs font-normal">
+            <p>
+              <strong>Key: </strong>{message.request.messageKey}
+              {#if message.request.action}<strong>Action: </strong>{message.request.action}{/if}
+              {#if message.request.actionText}<strong>Text: </strong>{message.request.actionText}{/if}
+              {#if message.request.gridUuid}<strong>GridUuid: </strong>{message.request.gridUuid}{/if}
+            </p>
+          </div>
+        </span>
+      </li>
+    {/if}
+    {#if message.response}
+      <li>
+        <span class="flex items-center">
+          <DownloadOutline color={message.response.status === SuccessStatus ? "green" : "red"} class="w-4 h-4" />
+          <div class="ps-4 text-xs font-normal">
+            <p>
+              <strong>Key: </strong>{message.response.messageKey}
+              {#if message.response.requestKey}<strong>Request: </strong>{message.response.requestKey}{/if}
+              {#if message.response.action}<strong>Action: </strong>{message.response.action}{/if}
+              {#if message.response.actionText}<strong>Text: </strong>{message.response.actionText}{/if}
+              {#if message.response.gridUuid}<strong>GridUuid: </strong>{message.response.gridUuid}{/if}
+              <strong>Status: </strong>{message.response.status}
+              <strong>Elapsed: </strong>{message.response.elapsedMs} ms
+            </p>
+          </div>
+        </span>
+      </li>        
+    {/if}
+  {/each}
+</ul>
+
 <style>
   li {
     list-style: none;
