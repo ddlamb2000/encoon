8f201dd78de68441c5a0ebc7315d5d3f6c5222ef
David Lambert <david.lambert@workday.com>
2025-01-19 15:15:55 +0100

    Display consumer count.

--- frontend/src/routes/[[dbName]]/pullMessages/+server.ts
+++ frontend/src/routes/[[dbName]]/pullMessages/+server.ts
@@ -2,6 +2,8 @@ import { env } from "$env/dynamic/private"
 import { kafka } from '$lib/kafka'
 import { newUuid } from "$lib/utils.svelte.ts"
 import * as metadata from "$lib/metadata.svelte"
+
+let consumerCount = 0
 	
 export const GET = async ({ params, request, url }) => {
   if(params.dbName === undefined) {
@@ -9,9 +11,9 @@ export const GET = async ({ params, request, url }) => {
     return new Response(JSON.stringify({ error: 'Missing dbName' }), { status: 500 })
   }
   const topic = env.TOPIC_PREFIX + "-" + params.dbName + "-responses"
-  const ac = new AbortController()
   const groupId = env.KAFKA_GROUP_ID + "-" + params.dbName + "-" + newUuid()
-  console.log(`GET ${url}: Create consumer using groupId ${groupId}`)
+  consumerCount += 1
+  console.log(`GET ${url}: Create consumer #${consumerCount} ${groupId}`)
   const consumer = kafka.consumer({
     groupId: groupId,
     minBytes: 20,
@@ -20,18 +22,19 @@ export const GET = async ({ params, request, url }) => {
     maxInFlightRequests: 50,
     retry: { retries: 5 }
   })
-  console.log(`PULL: Start streaming`)
+  console.log(`PULL: Start streaming #${consumerCount}`)
+  const ac = new AbortController()
   const stream = new ReadableStream({
     start(controller) {
       try {
-        console.log(`PULL: Submit an initialization message to the stream`)
+        console.log(`PULL #${consumerCount}: Submit an initialization message to the stream`)
         const initializationMessage = { key: metadata.InitializationKey }
         controller.enqueue(JSON.stringify(initializationMessage))
-        console.log(`PULL: Start connection for consumer`)
+        console.log(`PULL #${consumerCount}: Start connection for consumer`)
         consumer.connect()
-        console.log(`PULL: Consumer connected`)
+        console.log(`PULL #${consumerCount}: Consumer connected`)
         consumer.subscribe({ topics: [topic] })
-        console.log(`PULL: Consumer subscribed to ${topic}`)
+        console.log(`PULL #${consumerCount}: Consumer subscribed to ${topic}`)
         consumer.run({
             eachBatchAutoResolve: true,
             autoCommitInterval: 50,
@@ -45,10 +48,10 @@ export const GET = async ({ params, request, url }) => {
                 isStale,
                 pause,
             }) => {
-                console.log(`PULL: Consumer running from ${topic}`)
+                console.log(`PULL #${consumerCount}: Consumer running from ${topic}`)
                 for (let message of batch.messages) {
                   if (!isRunning() || isStale()) {
-                    console.log(`PULL: Consumer stopping from ${topic}`)
+                    console.log(`PULL #${consumerCount}: Consumer stopping from ${topic}`)
                     break
                   }
                   if(message.key !== null && message.value !== null) {
@@ -59,30 +62,30 @@ export const GET = async ({ params, request, url }) => {
                       key: message.key.toString(),
                       value: valueString
                     }
-                    console.log(`PULL (${valueString.length} bytes), key: ${message.key.toString()}`)
+                    console.log(`PULL #${consumerCount} (${valueString.length} bytes), key: ${message.key.toString()}`)
                     controller.enqueue(JSON.stringify(received))
                   } else {
-                    console.log(`PULL: Message with no key nor value`, message)
+                    console.log(`PULL #${consumerCount}: Message with no key nor value`, message)
                   }
-                  console.log(`PULL: Resolve offset from ${topic}: ${message.offset}`)
+                  console.log(`PULL #${consumerCount}: Resolve offset from ${topic}: ${message.offset}`)
                   resolveOffset(message.offset)
-                  console.log(`PULL: Awaiting heartbeat from ${topic}`)
+                  console.log(`PULL #${consumerCount}: Awaiting heartbeat from ${topic}`)
                   await heartbeat()
                 }
             },
         })
       } catch (error) {
-        console.error(`PULL: Error subscribe to Kafka:`, error)
+        console.error(`PULL #${consumerCount}: Error subscribe to Kafka:`, error)
         return new Response(JSON.stringify({ error }), { status: 500 })
       }
     },
     cancel() {
-      console.log(`PULL: Abort`)
+      console.log(`PULL #${consumerCount}: Abort streaming`)
       consumer.stop()
       consumer.disconnect()
       ac.abort()
     },
   })
-  console.log(`PULL: Return response as text/event-stream`)
+  console.log(`PULL #${consumerCount}: Return response as text/event-stream`)
   return new Response(stream, { headers: { 'Content-Type': 'text/event-stream' }, status: 200 })
 }
