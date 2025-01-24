3729d637f59de56395cd68c1a1f7a224ecb7c433
David Lambert <david.lambert@workday.com>
2025-01-24 08:09:52 +0100

    Change of operator.

--- frontend/.svelte-kit/types/src/routes/[[dbName]]/[[gridUuid]]/[[uuid]]/proxy+page.server.ts
+++ frontend/.svelte-kit/types/src/routes/[[dbName]]/[[gridUuid]]/[[uuid]]/proxy+page.server.ts
@@ -4,8 +4,8 @@ import type { PageServerLoad } from './$types'
 export const load = async ({ url, params }: Parameters<PageServerLoad>[0]) => {
   return {
     dbName: params.dbName,
-    gridUuid: params.gridUuid || "",
-    uuid: params.uuid || "",
+    gridUuid: params.gridUuid ?? "",
+    uuid: params.uuid ?? "",
     url: url.toString()
   }
 }
--- frontend/src/routes/[[dbName]]/[[gridUuid]]/[[uuid]]/+page.server.ts
+++ frontend/src/routes/[[dbName]]/[[gridUuid]]/[[uuid]]/+page.server.ts
@@ -3,8 +3,8 @@ import type { PageServerLoad } from './$types'
 export const load: PageServerLoad = async ({ url, params }) => {
   return {
     dbName: params.dbName,
-    gridUuid: params.gridUuid || "",
-    uuid: params.uuid || "",
+    gridUuid: params.gridUuid ?? "",
+    uuid: params.uuid ?? "",
     url: url.toString()
   }
 }
