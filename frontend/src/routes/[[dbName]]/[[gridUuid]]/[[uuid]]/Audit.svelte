65c5c9ecb2ac7a47c8948e4398fbcce8271bf996
David Lambert <david.lambert@workday.com>
2025-02-01 21:42:39 +0100

    Cleanup frontend with unused librairies.

--- frontend/src/routes/[[dbName]]/[[gridUuid]]/[[uuid]]/Audit.svelte
+++ frontend/src/routes/[[dbName]]/[[gridUuid]]/[[uuid]]/Audit.svelte
@@ -21,7 +21,7 @@
           <li class="font-extralight">
             {audit.actionName} on <DateTime dateTime={audit.created} />
             by
-            <a href="#" class="cursor-pointer font-light underline" onclick={() => context.navigateToGrid(metadata.UuidUsers, audit.createdBy)}>
+            <a href="#top" class="cursor-pointer font-light underline" onclick={() => context.navigateToGrid(metadata.UuidUsers, audit.createdBy)}>
               {audit.createdByName}
             </a>
           </li>
